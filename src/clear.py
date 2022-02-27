a69ca0e4d5d63b83ee2d43bb678a20940972cce2
Pascal Pirschel <pascal.pirschel@gmail.com>
2022-02-27 05:56:55 +0100

    Added switch to easily move between output types

--- src/clear.py
+++ src/clear.py
@@ -3,11 +3,18 @@
 import os
 from dotenv import load_dotenv
 from src.OutputModules.PPMOutput import PPMOutput
-
+from src.OutputModules.PiTFTOutput import PiTFTOutput
 
 load_dotenv()
 
 xSize = int(os.getenv('xSize', 240))
 ySize = int(os.getenv('ySize', 320))
-output = PPMOutput(xSize, ySize)
+outputType = os.getenv('output', 'ppm')
+if outputType == 'ppm':
+    output = PPMOutput(xSize, ySize)
+elif outputType == 'pitft':
+    output = PiTFTOutput(xSize, ySize)
+else:
+    raise ValueError(f'Unknown output type {outputType}')
+
 output.next_frame()
--- src/gradient.py
+++ src/gradient.py
@@ -4,13 +4,20 @@ import math
 import os
 from dotenv import load_dotenv
 from src.OutputModules.PPMOutput import PPMOutput
-
+from src.OutputModules.PiTFTOutput import PiTFTOutput
 
 load_dotenv()
 
 xSize = int(os.getenv('xSize', 240))
 ySize = int(os.getenv('ySize', 320))
-output = PPMOutput(xSize, ySize)
+outputType = os.getenv('output', 'ppm')
+if outputType == 'ppm':
+    output = PPMOutput(xSize, ySize)
+elif outputType == 'pitft':
+    output = PiTFTOutput(xSize, ySize)
+else:
+    raise ValueError(f'Unknown output type {outputType}')
+
 for y in range(0, ySize):
     for x in range(0, xSize):
         xVal = math.floor((x / xSize * 100))
